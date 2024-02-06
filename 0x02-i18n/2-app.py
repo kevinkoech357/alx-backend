a6eb4d599b3e5a08da5da2f83782d48394dd3047
Kevin Koech <kevinkoech@proton.me>
2024-02-06 18:48:11 +0300

    fix failing checks

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -34,7 +34,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-babel.init_app(app, locale_selector=get_locale)
+babel.init_app(app)
 
 
 @app.route("/", methods=["GET"])
