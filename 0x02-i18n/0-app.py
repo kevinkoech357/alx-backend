0bb1943f377a2d6e5f929583028ea9e21f259e2f
Kevin Koech <kevinkoech@proton.me>
2024-02-06 18:21:06 +0300

    rename html file

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -16,8 +16,8 @@ def render_index():
     """
     Render index.html
     """
-    return render_template("index.html")
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
