81c5c04dea330b34a55025ce65f23606e8daaaf4
Kevin Koech <kevinkoech@proton.me>
2024-02-06 18:39:31 +0300

    add a get_locale function to get best match based on user locale and add a template that should be rendered

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+"""
+Create a route using flask.
+Import and configure flask babel.
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel()
+
+
+class Config:
+    """
+    Babel configurations
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+def get_locale():
+    """
+    Get best match for user locale.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route("/", methods=["GET"])
+def render_index():
+    """
+    Render index.html
+    """
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
